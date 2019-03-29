a48bcaad9c526755b966058e1bea63107a011658
Matthew Gordon <inverarity@gmail.com>
2019-03-28 17:46:10 -0700

    Testier

--- test/test_flowchart.py
+++ test/test_flowchart.py
@@ -8,7 +8,7 @@ class TestFlowchart(TestCase):
     def test_base_class(self):
         f = FlowgistonChart()
         n1 = f.Generic.node("N1")
-        n1.edge(f.Generic.node("N2"))
+        n2 = n1.edge(f.Generic.node("N2"))
         with TemporaryDirectory() as td:
             f.save('test.gv', td)
             with open(os.path.join(td, 'test.gv'), 'r') as tf:
@@ -17,3 +17,12 @@ class TestFlowchart(TestCase):
                 self.assertIn('[label=N1 style=filled]', v[1])
                 self.assertIn('[label=N2 style=filled]', v[2])
                 self.assertIn('->', v[3])
+
+        n3 = n2.edge(f.Generic.if_("N3"), fillcolor='blue')
+        with TemporaryDirectory() as td:
+            f.save('test.gv', td)
+            with open(os.path.join(td, 'test.gv'), 'r') as tf:
+                v = tf.readlines()
+                n3_txt = [x for x in v if 'N3' in x]
+                self.assertEqual(len(n3_txt), 1)
+                self.assertIn('shape=diamond', n3_txt[0])
